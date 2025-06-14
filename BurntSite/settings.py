26fe1648d042e67aeca801f7c72fc7c2d68c324b
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2025-06-14 15:13:28 +0300

    nevermind

--- BurntSite/settings.py
+++ BurntSite/settings.py
@@ -41,10 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles'
 ]
-
-if DEBUG:
-    INSTALLED_APPS.append('django.contrib.staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = getenv("STATIC_ROOT")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
