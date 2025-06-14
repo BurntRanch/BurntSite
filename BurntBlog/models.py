3eaeea4c507c07f7b8421a4ecc9f4defed42889f
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2025-06-14 14:20:57 +0300

    i was supposed to gitignore .venv.. oops!

--- BurntBlog/models.py
+++ BurntBlog/models.py
@@ -5,4 +5,4 @@ class Post(models.Model):
     uid = models.UUIDField("Post UUID", primary_key=True, default=uuid4, unique=True, editable=False)
     title = models.CharField("Post Title")
     content = models.TextField("Post Content")
-    pub_date = models.DateTimeField("Date", auto_now=True)
+    pub_date = models.DateTimeField("Date", auto_now_add=True, editable=False)
